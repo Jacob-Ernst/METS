063bdbdacff823fe795197ee7bf87310fde5fa80
Jacob Ernst <akumadude.je@gmail.com>
2022-06-23 08:41:59 -0500

    Creating migrate command to simplify initializing and migrating fresh DB

---
+++ cmd/migrate/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"gitlab.com/jacob-ernst/mets/pkg/models"
+)
+
+func main() {
+	var dsn string
+	flag.StringVar(&dsn, "dsn", "data/dev.db", "dsn for the DB to migrate")
+	_, err := models.OpenDB(dsn)
+	if err != nil {
+		log.Fatalln("could not migrate dsn \"", dsn, "\":", err)
+	}
+}
